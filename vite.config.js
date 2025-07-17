aaa4d63e46654ab10bfd728c74f43b667745df19
love09pc <love09pc@naver.com>
2025-07-17 13:57:37 +0900

    초기 세팅

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/[portfolio/', // ★ 반드시 본인 레포 이름으로 변경
+  base: '/portfolio/', // ★ 반드시 본인 레포 이름으로 변경
 })
